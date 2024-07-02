2746c54c8268abb1ef10bc658a1128c8940250e8
Eduardaxavier <127114998+Eduardaxavier@users.noreply.github.com>
2024-07-02 09:47:09 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,17 +6,13 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
+        enunciado: "Quando a IA surgiu?",
         alternativas: [
             {
-                texto: "Isso é assustador!",
-                afirmacao: "No início ficou com medo do que essa tecnologia pode fazer. "
+                texto: "A IA surgiu em 1943,quando Warren McCulloch e Walter Pitts criaram o primeiro modelo computacional para redes neurais",
+                afirmacao: "No início a tecnologia não foi usada. "
             },
-            {
-                texto: "Isso é maravilhoso!",
-                afirmacao: "Quis saber como usar IA no seu dia a dia."
-            }
-        ]
+          ]
     },
     {
         enunciado: "Com a descoberta desta tecnologia, chamada Inteligência Artificial, uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre esta tecnologia. No fim de uma aula ela pede que você escreva um trabalho sobre o uso de IA em sala de aula. Qual atitude você toma?",
